38f54b41bfbc060caec1747ca4115350e5fee33c
matthijsvk <matthijsvankeirsbilck@gmail.com>
2016-11-22 11:55:04 +0100

    installed Caffe, finished phoneme/frame extraction (but still sync issue due to missing frames), MNIST convNet working and code refactoring, download TCD-TIMIT script removed protobuf

--- ImageSpeech/mouthDetection/visualizeROI.m
+++ ImageSpeech/mouthDetection/visualizeROI.m
@@ -8,32 +8,37 @@ close all;
 % h = imagesc(mat);
 % impixelregion
 
-%% This part is for the processed MAT file, containing only the valid frames
-
+% % debuggin weird error on si1618
+% validImages = {};
+% for i=1:length(validFrames)
+%     1+validFrames(i);
+%     validImages{i} = ROIs{1,1}{1,1+validFrames(i)};
+% end
+% 
+% %% This part is for the processed MAT file, containing only the valid frames
+% 
 % look for a silence 
-nbValid = length(validImages);
-validPhonemes2 = string(cellstr(validPhonemes));
-disp(class(validPhonemes2))
+% nbValid = length(validImages);
+% validPhonemes2 = string(cellstr(validPhonemes));
+% disp(class(validPhonemes2))
+% 
+% images = [];
+% for i=1:nbValid
+%    if (validPhonemes2{i} == string('sil'))  % s, 
+%        images(end+1) = i;
+%    end
+% end
+% display(images)
+% 
+% for i=1:length(images)
+%     figure;
+%     imagesc(validImages{images(i)});
+% end
 
-silences = [];
-for i=1:nbValid
-   if (validPhonemes2{i} == string('sil'))
-       silences(end+1) = i;
-   end
-end
-display(silences)
-
-for i=1:length(silences)
-    figure;
-    image = validImages{i};
-    h = imagesc(image);
-    %impixelregion
-end
-
-
-% % tests to see if images correspond
-% imagesc(validImages{2});
-% figure;
-% imagesc(ROIs{1}{19})
-% %spy(ROIs{1}{16} - validImages{1})
-
+% % % tests to see if images correspond
+% % imagesc(validImages{end});
+figure;
+imagesc(ROIs{1,1}{1,4})
+%spy(ROIs{1}{16} - validImages{1})
+% impixelregion
+% display(length(validImages))
