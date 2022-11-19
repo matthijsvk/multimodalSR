cc8ecf28364d069361de61ed54594bf6bb3dda58
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-19 23:30:34 +0000

    Adding tarfile member sanitization to extractall()

--- code/Experiments/neon-master/examples/faster-rcnn/ingest_pascalvoc.py
+++ code/Experiments/neon-master/examples/faster-rcnn/ingest_pascalvoc.py
@@ -50,7 +50,26 @@ def ingest_pascal(data_dir, out_dir, year='2007', overwrite=False):
     for file in tarfiles:
         with tarfile.open(file, 'r') as t:
             print("Extracting {} to {}".format(file, out_dir))
-            t.extractall(out_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(t, out_dir)
 
     # convert the annotations to json, exclude difficult objects (used for training)
     input_path = os.path.join(root_dir, 'Annotations')
--- code/Experiments/neon-master/neon/data/questionanswer.py
+++ code/Experiments/neon-master/neon/data/questionanswer.py
@@ -166,7 +166,26 @@ class BABI(Dataset):
 
         if os.path.exists(train_file) is False or os.path.exists(test_file):
             with tarfile.open(filepath, 'r:gz') as f:
-                f.extractall(workdir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(f, workdir)
 
         return train_file, test_file
 
